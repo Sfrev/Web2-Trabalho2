7ee90a1ab1b0e960da745559f484732a2bf4a681
Igor Machado <51010839+Sfrev@users.noreply.github.com>
2023-03-10 13:02:11 -0300

    react and tailwind init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-require('./controllers/authController')(app);
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
